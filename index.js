b2b205d44b9ef2cf5fec0e60a4ebec7215151c66
AipBorges <mpbds@uic.es>
2021-11-03 15:23:59 +0100

    chommit for process.argv implementation. function works

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const addition = (firstNumber, secondNumber) => {
-  firstNumber + secondNumber;
-};
-const substraction = (firstNumber, secondNumber) => {
-  firstNumber - secondNumber;
-};
-const multiplication = (firstNumber, secondNumber) => {
+const numbers = process.argv.slice(2);
+const firstNumber = numbers[0];
+const secondNumber = numbers[1];
+
+const addition = (firstNumber, secondNumber) => firstNumber + secondNumber;
+const substraction = (firstNumber, secondNumber) => firstNumber - secondNumber;
+const multiplication = (firstNumber, secondNumber) =>
   firstNumber * secondNumber;
-};
-const division = (firstNumber, secondNumber) => {
-  firstNumber / secondNumber;
-};
+const division = (firstNumber, secondNumber) => firstNumber / secondNumber;
 
 const calculator = (firstNumber, secondNumber) => {
-  return `${firstNumber}+${secondNumber} = ${addition}, ${firstNumber}-${secondNumber} = ${substraction}, ${firstNumber}*${secondNumber} = ${multiplication}, ${firstNumber}/${secondNumber} = ${division}`;
+  const added = addition(firstNumber, secondNumber);
+  const substracted = substraction(firstNumber, secondNumber);
+  const multiplied = multiplication(firstNumber, secondNumber);
+  const divided = division(firstNumber, secondNumber);
+
+  console.log(
+    `${firstNumber}+${secondNumber} = ${added}, 
+    ${firstNumber}-${secondNumber} = ${substracted}, 
+    ${firstNumber}*${secondNumber} = ${multiplied}, 
+    ${firstNumber}/${secondNumber} = ${divided}`
+  );
 };
 
-calculator(5, 3);
+calculator(firstNumber, secondNumber);
